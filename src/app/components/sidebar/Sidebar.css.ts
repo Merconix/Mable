068aed203b7b170e0cd11959a7385b4d32b0533b
Merc <41351832+Merconix@users.noreply.github.com>
2026-09-06 20:32:34 +0100

    Sidebar variable
    Sets to a sable variable for dynamic light/dark

--- src/app/components/sidebar/Sidebar.css.ts
+++ src/app/components/sidebar/Sidebar.css.ts
@@ -8,7 +8,7 @@ export const Sidebar = style([
   DefaultReset,
   {
     width: toRem(66),
-    backgroundColor: "#232323",
+    backgroundColor: "var(--sable-bg-sidebar)",
     borderRight: `${config.borderWidth.B300} solid ${color.Background.ContainerLine}`,
 
     display: 'flex',
